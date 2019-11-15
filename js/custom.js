44ff29807d4d9ee48dcc71db248d45c68b6d30c9
Sally <sallychen426@gmail.com>
2019-11-14 19:12:16 -0500

    change press.html

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,10 @@ jQuery(document).ready(function() {
 	
 	"use strict";
     $("#includedContent").load("newsContent.html");
+
+      $('.showSingle').click(function(){
+        $('.targetDiv').hide();
+        $('#div'+jQuery(this).attr('target')).show();
+        $(this).addClass('active').siblings().removeClass('active');
+    });
 });
