104e9be7922ae612618d0927e79544c23c384e9b
Sally <sallychen426@gmail.com>
2019-11-16 08:29:58 -0500

    modify staffs.html, borad.html

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,13 @@ jQuery(document).ready(function() {
 	
 	"use strict";
     $("#includedContent").load("newsContent.html");
+    $("#includedContent").load("newsContent915.html");
+
+     $('.showSingle').click(function(){
+        $('.targetDiv').hide();
+        $('#includedContent'+jQuery(this).attr('target')).show();
+        $(this).addClass('active').siblings().removeClass('active');
+    });
 
       $('.showSingle').click(function(){
         $('.targetDiv').hide();
